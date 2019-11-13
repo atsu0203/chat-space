38e81b3e2347eaeb8df0427f00cd0ff09490452c
atsu0203 <atsu0203@gmail.com>
2019-11-13 20:30:40 +0900

    add-incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
- 
+
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -10,7 +19,9 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-    def user_params
-      params.require(:user).permit(:name, :email)    
-    end
-  end 
+
+  def user_params
+    params.require(:user).permit(:name, :email)    
+  end
+
+end 
